b0810c630ed447b6906efc21a85243435fd8d236
Erfan-AB01 <erfan.abuee@gmail.com>
2023-06-01 10:50:03 +0330

    project structure

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+
+
+public class Main {
+    public static void main(String[] args){
+        Menu menu = new Menu();
+        System.out.println("hello");
+        menu.insert("one",1);
+        System.out.println(menu);
+    }
+
+
+
+}
---
+++ src/Menus.java
@@ -0,0 +1,16 @@
+public class Menus {
+
+    public void mainMenuAppearance(){
+        System.out.println("""
+                               Airline Reservation System
+                            <1> Sign up
+                            <2> Sign in
+                            <3> Exit
+                """);
+
+
+    }
+
+
+
+}
